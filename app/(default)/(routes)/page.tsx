f465c9102309c79098dde8c0cc5fb021a62986de
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2024-01-09 23:37:35 +0100

    feat: homepage welcome message added

--- app/(default)/(routes)/page.tsx
+++ app/(default)/(routes)/page.tsx
@@ -1,3 +1,17 @@
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Terminal } from "lucide-react";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div className="w-full">
+      <Alert>
+        <Terminal className="h-4 w-4" />
+        <AlertTitle>Hoş geldiniz!</AlertTitle>
+        <AlertDescription>
+          Bu uygulama Fatih Sultan Mehmet Vakıf Üniversitesi öğrencilerinin
+          ihtiyaçları göz önüne alınarak geliştirilmektedir.
+        </AlertDescription>
+      </Alert>
+    </div>
+  );
 }
