24f063934c8d66cb4824c167ecec2a34e747bcc7
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2024-01-10 19:18:32 +0100

    feat: logo component developed

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -1,19 +1,13 @@
 import Link from "next/link";
 import DefaultFooter from "@/components/theme/footer";
+import ThemeLogo from "@/components/theme/logo";
 
 const DefaultLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full flex flex-col">
       <div className="w-full border-b py-4 flex justify-center">
         <div className="content-max-width flex">
-          <div className="">
-            <Link
-              href="/"
-              className="hover:text-gray-700 transition-colors font-semibold"
-            >
-              fsmvu
-            </Link>
-          </div>
+          <ThemeLogo />
           <div className="ml-auto flex gap-4 text-sm">
             <Link href="/signin">Giriş Yap</Link>
             <Link href="/signup">Üye Ol</Link>
--- components/theme/footer.tsx
+++ components/theme/footer.tsx
@@ -2,20 +2,14 @@ import { Github, Instagram } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 import { version } from "@/package.json";
+import ThemeLogo from "@/components/theme/logo";
 
 export default function DefaultFooter() {
   return (
     <div className="w-full border-t py-4 mt-auto flex justify-center">
       <div className="content-max-width flex flex-col gap-2">
         <div className="w-full flex justify-between">
-          <div className="">
-            <Link
-              href="/"
-              className="hover:text-gray-700 transition-colors font-semibold"
-            >
-              fsmvu
-            </Link>
-          </div>
+        <ThemeLogo />
         </div>
         <div className="w-full grid grid-cols-3 items-center gap-10">
           <div className="flex gap-1 text-xs text-gray-700">
