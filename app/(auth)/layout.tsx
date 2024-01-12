4a1bbd87664b36fde4ef19e98c47093f7efa6bbb
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2024-01-12 16:42:33 +0100

    feat: update auth layout footer

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,4 @@
-import Link from "next/link";
-import { Github } from "lucide-react";
+import DefaultFooter from "@/components/theme/footer";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -7,27 +6,7 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => {
       <div className="h-full flex justify-center items-center">
         <div className="content-max-width my-4 flex justify-center">{children}</div>
       </div>
-      <div className="w-full py-4 mt-auto flex justify-center">
-        <div className="content-max-width flex">
-          <div className="">
-            <Link
-              href="/"
-              className="hover:text-gray-700 transition-colors font-semibold"
-            >
-              fsmvu
-            </Link>
-          </div>
-          <div className="ml-auto">
-            <Link
-              href="https://github.com/bedirhanyildirim/fsmvu.com"
-              className="hover:text-gray-700 transition-colors"
-              target="_blank"
-            >
-              <Github />
-            </Link>
-          </div>
-        </div>
-      </div>
+      <DefaultFooter />
     </div>
   );
 };
