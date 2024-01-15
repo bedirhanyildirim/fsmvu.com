b18c1c6c38d952c051ab6101f49748f5c2298bec
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2024-01-15 19:32:42 +0100

    feat: check email input validity

--- app/(auth)/(routes)/signup/page.tsx
+++ app/(auth)/(routes)/signup/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   CardTitle,
   CardDescription,
@@ -6,13 +7,45 @@ import {
   CardFooter,
   Card,
 } from "@/components/ui/card";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
 import ThemeLogo from "@/components/theme/logo";
+import Link from "next/link";
+import { AlertCircle } from "lucide-react";
+import { useState } from "react";
+
+type FormData = {
+  email: string;
+  password: string;
+};
 
 const PageSignUp = () => {
+  const [formData, setFormData] = useState<FormData>({
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState<boolean>(false);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (): void => {
+    if (!/^[a-zA-Z]+\.[a-zA-Z]+@fsm\.edu\.tr$/.test(formData.email)) {
+      setError(true);
+      return;
+    } else {
+      setError(false);
+    }
+    console.log("Form gönderildi:", formData);
+  };
+
   return (
     <main className="flex flex-col items-center justify-center w-full">
       <Card className="w-full max-w-md px-4 py-8">
@@ -25,6 +58,15 @@ const PageSignUp = () => {
           </CardDescription>
         </CardHeader>
         <CardContent className="space-y-4">
+          {error && (
+            <Alert variant="destructive">
+              <AlertCircle className="h-4 w-4" />
+              <AlertTitle>Geçersiz email adresizi</AlertTitle>
+              <AlertDescription>
+                Sadece üniversite email adresinizi kullanarak üye olabilirsiniz.
+              </AlertDescription>
+            </Alert>
+          )}
           <div className="space-y-2">
             <Label htmlFor="email">Email</Label>
             <Input
@@ -32,15 +74,25 @@ const PageSignUp = () => {
               placeholder="ad.soyad@fsm.edu.tr"
               required
               type="email"
+              name="email"
+              onChange={handleInputChange}
             />
           </div>
           <div className="space-y-2">
             <Label htmlFor="password">Şifre</Label>
-            <Input id="password" required type="password" />
+            <Input
+              id="password"
+              name="password"
+              onChange={handleInputChange}
+              required
+              type="password"
+            />
           </div>
         </CardContent>
         <CardFooter>
-          <Button className="w-full">Üye Ol</Button>
+          <Button onClick={handleSubmit} className="w-full">
+            Üye Ol
+          </Button>
         </CardFooter>
       </Card>
       <div className="mt-4 text-center">
